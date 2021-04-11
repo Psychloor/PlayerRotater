06599851b83727cd6200fd674a3eb9eb64dd5df7
Psychloor <blomqvistkevin@hotmail.com>
2021-04-12 00:41:43 +0200

    Bump Version

--- PlayerRotater/BuildInfo.cs
+++ PlayerRotater/BuildInfo.cs
@@ -12,7 +12,9 @@ namespace PlayerRotater
 
         public const string Name = "Player Rotater";
 
-        public const string Version = "1.2.2";
+        // 1.3.0 control schemes and rotation origins
+        // Haven't released this version yet
+        public const string Version = "1.3.0";
 
     }
 
